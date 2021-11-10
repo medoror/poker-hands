40485d49034b5238600d92d49c96aec24d4a1aed
Michael Edoror <michael.o.edoror.ctr@mail.mil>
2021-11-10 17:29:43 -0600

    WIP: Removes old test classes from controller

--- src/test/java/workshop/pokerhands/PokerHandsControllerTest.java
+++ src/test/java/workshop/pokerhands/PokerHandsControllerTest.java
@@ -2,9 +2,10 @@ package workshop.pokerhands;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
@@ -16,46 +17,30 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(PokerHandsController.class)
 public class PokerHandsControllerTest {
 
     @Autowired
     MockMvc mockMvc;
 
-    @Autowired
+    @MockBean
     PokerHandService pokerHandService;
 
     @Test
-    public void shouldResultInADrawWhenNeitherHandWins() throws Exception {
-
-        Integer[] firstHand = new Integer[]{7,5,4,3,2};
-        Integer[] secondHand = new Integer[]{7,5,4,3,2};
-
-        List<Integer[]> roundHands = List.of(firstHand, secondHand);
-
-        MockHttpServletRequestBuilder request = get("/pokerHandResult")
-                        .content( new ObjectMapper().writeValueAsString(roundHands))
-                .contentType(MediaType.APPLICATION_JSON);
-
-        mockMvc.perform(request)
-                .andExpect(status().isOk())
-                .andExpect(content().string("Draw"));
-    }
-
-    @Test
-    public void shouldDeclareTheHandWithTheHigherCardTheWinner() throws Exception {
+    public void shouldCompareHandsWhenEndpointIsCalled() throws Exception {
 
         Integer[] firstHand = new Integer[]{2,5,4,3,8};
         Integer[] secondHand = new Integer[]{7,5,4,3,2};
-
         List<Integer[]> roundHands = List.of(firstHand, secondHand);
+
+        Mockito.when(pokerHandService.compareHands(firstHand, secondHand)).thenReturn("Draw");
+
         MockHttpServletRequestBuilder request = get("/pokerHandResult")
                 .content( new ObjectMapper().writeValueAsString(roundHands))
                 .contentType(MediaType.APPLICATION_JSON);
 
         mockMvc.perform(request)
                 .andExpect(status().isOk())
-                .andExpect(content().string("First Hand Wins"));
+                .andExpect(content().string("Draw"));
     }
 }
